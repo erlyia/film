2245adb06cc9fa3382fd96254037b879c236edb9
Erika <erika.yang46@gmail.com>
2022-12-25 19:18:12 -0500

    show image

--- src/pages/Filmography.js
+++ src/pages/Filmography.js
@@ -1,9 +1,29 @@
+import aboutPhoto from './IMG_5008.png';
+
 function Filmography() {
 
     return (
-        <div className="about">
-            <div className="nav">
-                test
+        <div className="container">
+            <div className="vertical-container">
+                <div className="horizontal-container">
+                    <div className="shorts">
+                    <b>Explorations</b>
+                    <br></br>
+                    <em>where i experiment</em>
+                    <br></br>
+                    <br></br>
+                    <p> <span className="hiddentxt">a reprise </span>
+                    <span className="hiddenimg"> <img src={aboutPhoto} alt="about" width="20%"/> </span>
+                    </p>
+                    <br></br>
+                    we're all mad here
+                    <br></br>
+                    bing bang bop
+                    <br></br>
+                    montauk in november
+                    </div>
+                </div>
+                
             </div>
             <div className="body">
 
