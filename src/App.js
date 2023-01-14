afb2dd40f4e8c191039f534e7d2192816dc491fa
Erika <erika.yang46@gmail.com>
2023-01-13 19:42:52 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Cinematography from './pages/Cinematography.js';
 import Filmography from './pages/Filmography.js';
 import Writing from './pages/Writing.js';
 import Navbar from './components/Navbar.js';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { HashRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
     
-    <BrowserRouter>
+    <HashRouter>
     <Navbar />
     <Routes>
-      <Route basename={process.env.PUBLIC_URL} path="/" element={<About/>} />
-      <Route basename={process.env.PUBLIC_URL} path="/about" element={<About/>} />
-      <Route basename={process.env.PUBLIC_URL} path="/cinematography" element={<Cinematography/>} />
-      <Route basename={process.env.PUBLIC_URL} path="/writing" element={<Writing/>} />
-      <Route basename={process.env.PUBLIC_URL} path="/filmography" element={<Filmography/>} />
+      <Route path="/" element={<About/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/cinematography" element={<Cinematography/>} />
+      <Route path="/writing" element={<Writing/>} />
+      <Route path="/filmography" element={<Filmography/>} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </>
   );
 }
